f40e9903a72a8259ce7a2073e0fa7c424d1163a6
frithjof <frithjofrensen@gmail.com>
2024-01-17 18:14:20 +0100

    addieren, subtrahieren funktionen hinzugefügt

--- taschenrechner.py
+++ taschenrechner.py
@@ -1,1 +1,13 @@
 operation = input("Bitte Rechenoperation auswählen:\n1:Addieren\n2:Subtrahieren\n3:Multiplizieren\4:Dividieren")
+
+def subtrahieren(num1, num2):
+    ergebnis = num1 - num2
+    return ergebnis
+
+
+
+def addieren(num1, num2):
+    ergebnis = num1 + num2
+    return ergebnis
+    
+        
