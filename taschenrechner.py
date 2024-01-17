60c5f20195ab075194fa1baa83259944c3a34a36
Michael <michael.martynov03@gmail.com>
2024-01-17 15:19:52 +0100

    commit von b

--- taschenrechner.py
+++ taschenrechner.py
@@ -1,1 +1,3 @@
 operation = input("Bitte Rechenoperation auswählen:\n1:Addieren\n2:Subtrahieren\n3:Multiplizieren\4:Dividieren")
+print("Das stammt von branch b")
+#zusätlich noch ein Kommentar von b
