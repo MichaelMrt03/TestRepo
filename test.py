e400ba53ac9961d6bf9c61a1c7d991f1047fffcd
Lucas <lucas.keller156@hotmail.com>
2024-01-18 21:54:33 +0100

    test.py löschen

--- test.py
+++
@@ -1,8 +0,0 @@
-    #Methoden zur Multiplikation/Division
-def multiplizieren(num1, num2):
-    ergebnis = num1 * num2
-    return ergebnis
-
-def dividieren(num1, num2):
-    ergebnis = num1 / num2
-    return ergebnis
